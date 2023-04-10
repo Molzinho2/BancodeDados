a420f206fb7eec9ba8a852e0c51c95a492744d7d
igor- <igor-@IGORMOL>
2023-04-10 08:39:30 -0300

    Adicionar arquivos de projeto.

---
+++ BancodeDados/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace BancodeDados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnCriarTabela = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnEditar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelResultado
+            // 
+            resources.ApplyResources(this.labelResultado, "labelResultado");
+            this.labelResultado.Name = "labelResultado";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txtNome
+            // 
+            resources.ApplyResources(this.txtNome, "txtNome");
+            this.txtNome.Name = "txtNome";
+            // 
+            // txtEmail
+            // 
+            resources.ApplyResources(this.txtEmail, "txtEmail");
+            this.txtEmail.Name = "txtEmail";
+            // 
+            // btnConectar
+            // 
+            resources.ApplyResources(this.btnConectar, "btnConectar");
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnCriarTabela
+            // 
+            resources.ApplyResources(this.btnCriarTabela, "btnCriarTabela");
+            this.btnCriarTabela.Name = "btnCriarTabela";
+            this.btnCriarTabela.UseVisualStyleBackColor = true;
+            this.btnCriarTabela.Click += new System.EventHandler(this.btnCriarTabela_Click);
+            // 
+            // btnInserir
+            // 
+            resources.ApplyResources(this.btnInserir, "btnInserir");
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnProcurar
+            // 
+            resources.ApplyResources(this.btnProcurar, "btnProcurar");
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // btnExcluir
+            // 
+            resources.ApplyResources(this.btnExcluir, "btnExcluir");
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // lista
+            // 
+            this.lista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.lista.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.lista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nome,
+            this.email});
+            resources.ApplyResources(this.lista, "lista");
+            this.lista.Name = "lista";
+            this.lista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            // 
+            // id
+            // 
+            resources.ApplyResources(this.id, "id");
+            this.id.Name = "id";
+            // 
+            // nome
+            // 
+            resources.ApplyResources(this.nome, "nome");
+            this.nome.Name = "nome";
+            // 
+            // email
+            // 
+            resources.ApplyResources(this.email, "email");
+            this.email.Name = "email";
+            // 
+            // btnEditar
+            // 
+            resources.ApplyResources(this.btnEditar, "btnEditar");
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnProcurar);
+            this.Controls.Add(this.btnCriarTabela);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelResultado);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnCriarTabela;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView lista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.Button btnEditar;
+    }
+}
+
---
+++ BancodeDados/Form1.cs
@@ -0,0 +1,461 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.IO;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace BancodeDados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            #region SQLite
+            /*
+            string BasedeDados = Application.StartupPath + @"\db\DBSQLite.db";
+            string strConection = @"Data Source = " + BasedeDados + "; Version = 3";
+
+            if(!File.Exists(BasedeDados))
+            {
+                SQLiteConnection.CreateFile(BasedeDados);
+            }
+
+            SQLiteConnection conexao = new SQLiteConnection(strConection);
+
+            try
+            {
+                conexao.Open();
+                labelResultado.Text = "Conectdo com sucesso ao SQlite!";
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Erro ao Conectar SQlite \n" + ex;
+            }
+            finally { conexao.Close(); }
+            */
+            #endregion
+
+            #region MySQL
+            
+            string strConnection = @"server=localhost;User Id=root;database=mysqldb;password=";
+
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+            try
+            {
+                conexao.Open();
+                labelResultado.Text = "Conexão ao MySQL realizada com sucesso!";
+
+                MySqlCommand comando = new MySqlCommand();
+
+                comando.Connection = conexao;
+
+                comando.CommandText = "CREATE DATABASE IF NOT EXISTS MySqlDB";
+
+                comando.ExecuteNonQuery();
+                labelResultado.Text = "Base de Dados criada com sucesso!!";
+                comando.Dispose();
+                
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Erro ao Conectar ao MySQL \n" + ex;
+            }
+            finally { conexao.Close(); }
+            
+            #endregion
+        }
+
+        private void btnCriarTabela_Click(object sender, EventArgs e)
+        {
+            #region SQLite
+            /*
+            string BasedeDados = Application.StartupPath + @"\db\DBSQLite.db";
+            string strConnection = @"Data Source = " + BasedeDados + "; Version = 3";
+
+            SQLiteConnection conexao = new SQLiteConnection(strConnection);
+
+
+            try
+            {
+                conexao.Open();
+
+                SQLiteCommand comando = new SQLiteCommand();
+                comando.Connection = conexao;
+
+                comando.CommandText = "CREATE TABLE pessoas ( id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, nome VARCHAR(50), email VARCHAR(50))";
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Tabela criada com sucesso!";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel criar a tabela pessoas! \n" + ex;
+                
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            */
+            #endregion
+
+            #region MySQl
+
+            string strConnection = @"server=localhost;User Id=root;database=mysqldb;password=";
+
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+
+            try
+            {
+                conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+
+                comando.CommandText = "CREATE TABLE pessoas (id INTEGER NOT NULL AUTO_INCREMENT, nome VARCHAR(50), email VARCHAR(50), PRIMARY KEY (id))";
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Tabela criada com sucesso!";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel criar a tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            #endregion
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            #region SQLite
+            /*
+            string BasedeDados = Application.StartupPath + @"\db\DBSQLite.db";
+            string strConnection = @"Data Source = " + BasedeDados + "; Version = 3";
+
+            SQLiteConnection conexao = new SQLiteConnection(strConnection);
+
+            try
+            {
+                conexao.Open();
+
+                SQLiteCommand comando = new SQLiteCommand();
+                comando.Connection = conexao;
+
+                int id = new Random(DateTime.Now.Millisecond).Next(0, 1000);
+                string nome = txtNome.Text; 
+                string email = txtEmail.Text;
+
+                comando.CommandText = String.Format("INSERT INTO pessoas VALUES ('{0}','{1}','{2}')", id,nome, email);
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Valores inseridos com sucesso na tabela pessoas!";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel inserir os valores na tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            */
+            #endregion
+
+            #region MySQL
+            
+            string strConnection = @"server=localhost;User Id=root;database=mysqldb;password=";
+
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+            try
+            {
+                conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+
+                int id = new Random(DateTime.Now.Millisecond).Next(0, 1000);
+                string nome = txtNome.Text;
+                string email = txtEmail.Text;
+
+                comando.CommandText = String.Format("INSERT INTO pessoas VALUES ('{0}','{1}', '{2}')",id, nome, email);
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Valores inseridos com sucesso na tabela pessoas!";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel inserir os valores na tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            
+            #endregion
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            #region SQLite
+            /*
+            string BasedeDados = Application.StartupPath + @"\db\DBSQLite.db";
+            string strConnection = @"Data Source = " + BasedeDados + "; Version = 3";
+
+            SQLiteConnection conexao = new SQLiteConnection(strConnection);
+
+            try
+            {
+                String query = "SELECT * FROM pessoas";
+
+                if (txtNome.Text != "")
+                {
+                    query = "SELECT * FROM pessoas WHERE nome LIKE '" + txtNome.Text + "'";
+                }
+
+                DataTable dados = new DataTable();
+                SQLiteDataAdapter adaptador = new SQLiteDataAdapter(query, strConnection);
+
+                conexao.Open();
+
+                adaptador.Fill(dados);
+
+                lista.Rows.Clear();
+
+                foreach (DataRow item in dados.Rows)
+                {
+                    lista.Rows.Add(item.ItemArray);
+                }
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel procurar os dados na tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            */
+            #endregion
+
+            #region MySQL
+            
+            string strConnection = @"server=localhost;User Id=root;database=mysqldb;password=";
+
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+            try
+            {
+                String query = "SELECT * FROM pessoas";
+
+                if (txtNome.Text != "")
+                {
+                    query = "SELECT * FROM pessoas WHERE nome LIKE '" + txtNome.Text + "'";
+                }
+
+                DataTable dados = new DataTable();
+                MySqlDataAdapter adaptador = new MySqlDataAdapter(query, strConnection);
+
+                conexao.Open();
+
+                adaptador.Fill(dados);
+                lista.Rows.Clear();
+
+                foreach (DataRow item in dados.Rows)
+                {
+                    lista.Rows.Add(item.ItemArray);
+                }
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel procurar os dados na tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            
+            #endregion
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            #region SQLite
+            /*
+            string BasedeDados = Application.StartupPath + @"\db\DBSQLite.db";
+            string strConnection = @"Data Source = " + BasedeDados + "; Version = 3";
+
+            SQLiteConnection conexao = new SQLiteConnection(strConnection);
+
+            try
+            {
+                conexao.Open();
+
+                SQLiteCommand comando = new SQLiteCommand();
+                comando.Connection = conexao;
+
+                int id = (int)lista.SelectedRows[0].Cells[0].Value;
+                comando.CommandText = String.Format("DELETE FROM pessoas WHERE id = '{0}'", id);
+
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Registo excluido com sucesso!!";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel excluir o dado na tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            */
+            #endregion
+
+            #region MySQL
+            
+            string strConnection = @"server=localhost;User Id=root;database=mysqldb;password=";
+
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+            try
+            {
+                conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+
+                int id = (int)lista.SelectedRows[0].Cells[0].Value;
+                comando.CommandText = String.Format("DELETE FROM pessoas WHERE id = '{0}'", id);
+
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Registo excluido com sucesso!!";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel excluir o dado na tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            
+            #endregion
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            #region SQLite
+            /*
+            string BasedeDados = Application.StartupPath + @"\db\DBSQLite.db";
+            string strConnection = @"Data Source = " + BasedeDados + "; Version = 3";
+
+            SQLiteConnection conexao = new SQLiteConnection(strConnection);
+
+            try
+            {
+                conexao.Open();
+
+                SQLiteCommand comando = new SQLiteCommand();
+                comando.Connection = conexao;
+
+                //int id = (int)lista.SelectedRows[0].Cells[0].Value;
+                string nome = txtNome.Text;
+                string email = txtEmail.Text;
+
+                //comando.CommandText = String.Format("UPDATE pessoas SET nome = '{0}', email = '{1}' WHERE id = '{2}'",nome, email, id);
+
+                //comando.ExecuteNonQuery();
+
+                labelResultado.Text = id.ToString();
+                //comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel alterar os dados na tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            */
+            #endregion
+            
+            #region MySQL
+            
+            string strConnection = @"server=localhost;User Id=root;database=mysqldb;password=";
+
+            MySqlConnection conexao = new MySqlConnection(strConnection);
+
+            try
+            {
+                conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexao;
+
+                int id = (int)lista.SelectedRows[0].Cells[0].Value;
+                string nome = txtNome.Text;
+                string email = txtEmail.Text;
+
+                comando.CommandText = String.Format("UPDATE pessoas SET nome = '{0}', email = '{1}' WHERE id = '{2}'", nome, email, id);
+
+                comando.ExecuteNonQuery();
+
+                labelResultado.Text = "Registro alterado com sucesso!";
+                comando.Dispose();
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Não foi possivel procurar os dados na tabela pessoas! \n" + ex;
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            
+            #endregion
+        }
+    }
+}
